c4e7f402b4c6bf5bb30690e103ec26275d10e916
Rich <rclt201@gmail.com>
2021-05-16 20:28:48 +0100

    fix: move navbar to the right

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,15 +4,8 @@ import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import {
   Box,
   Flex,
-  Avatar,
   HStack,
   IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
   Stack,
@@ -31,10 +24,10 @@ const NavLink = ({ link }: { link: LinkType }) => (
       px={2}
       py={1}
       rounded="md"
+      cursor="pointer"
       _hover={{
         textDecoration: 'none',
         bg: useColorModeValue('gray.200', 'gray.700'),
-        cursor: 'pointer',
       }}
     >
       {link.label}
@@ -57,27 +50,13 @@ export const NavBar = ({ links }: { links: LinkType[] }) => {
         />
         <HStack spacing={8} alignItems="center">
           <Box>Logo</Box>
+        </HStack>
+        <Flex alignItems="center">
           <HStack as="nav" spacing={4} display={{ base: 'none', md: 'flex' }}>
             {links.map((link) => (
               <NavLink key={link.href} link={link} />
             ))}
           </HStack>
-        </HStack>
-        <Flex alignItems="center">
-          <Menu>
-            <MenuButton as={Button} rounded="full" variant="link" cursor="pointer">
-              <Avatar
-                size="sm"
-                src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-              />
-            </MenuButton>
-            <MenuList>
-              <MenuItem>Link 1</MenuItem>
-              <MenuItem>Link 2</MenuItem>
-              <MenuDivider />
-              <MenuItem>Link 3</MenuItem>
-            </MenuList>
-          </Menu>
         </Flex>
       </Flex>
 
